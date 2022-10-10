534100d68c1ee4fc6ad637944ad3b044ad202dc8
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-10 04:45:53 -0400

    Kivy now outouts year to screen

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.slider import Slider
 
-print("foo")
-
 Builder.load_file('slider.kv')
 
 
 
 class MyLayout(Widget):
     def slide_it(self, *args):
-        print(args[1])
+        # print(args[1])
+        self.slide_text.text = str(int(args[1]))
 
 class AwesomeApp(App):
     def build(self):
